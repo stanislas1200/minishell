5273d4a933c949141dbab0b9a8458b3776950bb9
Danilo Spillebeen <dspilleb@shi-r2-p3.s19.be>
2023-08-20 16:35:24 +0200

    execve instead of execvp

---
+++ execve.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   execve.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dspilleb <dspilleb@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/20 15:51:53 by dspilleb          #+#    #+#             */
+/*   Updated: 2023/08/20 16:35:05 by dspilleb         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+char	*create_command_path(char *path, char *command)
+{
+	char	*tmp;
+	char	*full_path;
+
+	tmp = ft_strjoin("/", command);
+	if (!tmp)
+		return (NULL);
+	full_path = ft_strjoin(path, tmp);
+	free(tmp);
+	return (full_path);
+}
+
+char	*find_command_path(char *all_paths, char *command)
+{
+	int		i;
+	char	**arr;
+	char	*full_path;
+
+	i = -1;
+	if (!all_paths || !command || !ft_strlen(command))
+		return (NULL);
+	arr = ft_split(all_paths, ':');
+	if (!arr)
+		return (cperror("Malloc", NULL, NULL, 1), NULL);
+	while (arr && arr[++i])
+	{
+		full_path = create_command_path(arr[i], command);
+		if (full_path && access(full_path, X_OK) == 0)
+		{
+			free_matrix(arr);
+			return (full_path);
+		}
+		if (full_path)
+			free(full_path);
+	}
+	free_matrix(arr);
+	return (ft_strdup(command));
+}
+
+void	ft_execve(char **env, char *cmd, char **args)
+{
+	char	*cmd_path;
+
+	cmd_path = find_command_path(ft_getenv(env, "PATH"), cmd);
+	execve(cmd_path, args, env);
+	free(cmd_path);
+	free(args);
+	cperror("execve", NULL, NULL, 1);
+	exit(1);
+}
